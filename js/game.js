60369fe5a368b24d008bda653448be465a2b005b
Stefan Cosma <stefcosma@gmail.com>
2015-04-14 18:53:29 +0000

    Minor changes

--- js/game.js
+++ js/game.js
@@ -3,7 +3,7 @@ var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
-canvas.className = "block";
+canvas.className = "wrapper";
 document.body.appendChild(canvas);
 
 // Background image
